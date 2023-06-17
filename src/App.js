9e655e94a075290db045c967bce7eb698dc92e2b
Ankit-191 <godaraankit003@gmail.com>
2023-06-17 12:33:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Hero from "./components/Hero";
+import Ourportfolios from "./components/Ourportfolios";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Utn from "./components/Utn";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Ourportfolios/>
+      <Utn />
     </div>
   );
 }
